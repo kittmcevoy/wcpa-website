5d583c14e514a4f418f8f48aac0ce89812d70a31
kittmcevoy <kitt.mcevoy@outlook.com>
2025-02-14 12:11:12 +1300

    updated committee page with committee names and photos.

--- src/Committee.jsx
+++ src/Committee.jsx
@@ -1,7 +1,12 @@
 import Header from "./Header.jsx";
-import PresidentPhoto from "./images/WCPA_logo.png"
-import TreasurerPhoto from "./images/WCPA_logo.png"
-import SecetaryPhoto from "./images/WCPA_logo.png"
+import PresidentPhoto from "./images/WCPA_logo.png";
+import TreasurerPhoto from "./images/WCPA_logo.png";
+import SecretaryPhoto from "./images/WCPA_logo.png";
+import SocialLeagueRepPhoto from "./images/Committee Photos/Social League Rep Photo.jpg";
+import GearRepPhoto from "./images/Committee Photos/Gear Rep Photo.jpg";
+import SchoolsRepPhoto from "./images/Committee Photos/Schools Rep Photo.JPG";
+import SquadManagerPhoto from "./images/WCPA_logo.png";
+import FundraisingManagerPhoto from "./images/WCPA_logo.png";
 function Committee() {
     return (
       <div>
@@ -10,15 +15,15 @@ function Committee() {
         <div>
           <div className="flex flex-row justify-evenly py-10">
             <CommitteeMember name="Charles Irvine-Ford" position="President" photo={PresidentPhoto} email="wgtncanoepolo@gmail.com"/>
-            <CommitteeMember name="Micheal Bird" position="Treasure" photo={PresidentPhoto} email="wgtncanoepolo@gmail.com"/>
-            <CommitteeMember name="Kitt McEvoy" position="Secetary" photo={PresidentPhoto} email="wgtncanoepolo@gmail.com"/>
-            <CommitteeMember name="Kate McGrath" position="School Development" photo={PresidentPhoto} email="wgtncanoepolo@gmail.com"/>
+            <CommitteeMember name="Micheal Bird" position="Treasure" photo={TreasurerPhoto} email="wgtncanoepolo@gmail.com"/>
+            <CommitteeMember name="Kitt McEvoy" position="Secretary" photo={SecretaryPhoto} email="wcpasecretary@gmail.com"/>
+            <CommitteeMember name="Kate McGrath" position="School Development" photo={SchoolsRepPhoto} email="kate.mcgrath@outlook.com"/>
           </div>
           <div className="flex flex-row justify-evenly">
-            <CommitteeMember name="Thomas McDermott" position="Squad Manager" photo={PresidentPhoto} email="wgtncanoepolo@gmail.com"/>
-            <CommitteeMember name="Josh Bird" position="Social League" photo={PresidentPhoto} email="wgtncanoepolo@gmail.com"/>
-            <CommitteeMember name="Adam Bird" position="Cant remember" photo={PresidentPhoto} email="wgtncanoepolo@gmail.com"/>
-            <CommitteeMember name="Jack Arcus" position="Gear Manager" photo={PresidentPhoto} email="wgtncanoepolo@gmail.com"/>
+            <CommitteeMember name="Thomas McDermott" position="Squad Manager" photo={SquadManagerPhoto} email="wgtncanoepolo@gmail.com"/>
+            <CommitteeMember name="Josh Bird" position="Social League" photo={SocialLeagueRepPhoto} email="wgtncanoepolo@gmail.com"/>
+            <CommitteeMember name="Adam Bird" position="Gear Manager" photo={GearRepPhoto} email="wgtncanoepolo@gmail.com"/>
+            <CommitteeMember name="Jack Arcus" position="Fundraising Manager" photo={FundraisingManagerPhoto} email="wgtncanoepolo@gmail.com"/>
           </div>
         </div>
           
